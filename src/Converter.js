58c05aac6901f91d0f9d8403a09079df01f48d0b
Gia1987 <giacomo.n27@gmail.com>
2018-07-05 20:02:50 +0100

    first test pass. green

---
+++ src/Converter.js
@@ -0,0 +1,11 @@
+function Converter(){
+
+}
+
+Converter.prototype.convert = function(num){
+  return 'I'
+}
+
+module.exports = Converter;
+// var romans = new Romans()
+// romans.convertToRomans(2648)
